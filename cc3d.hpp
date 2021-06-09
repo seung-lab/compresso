845da28bb062724cdfd6a4fcac49da8507eb11a3
William Silversmith <william.silversmith@gmail.com>
2021-06-09 15:25:14 -0400

    chore: update license for cc3d.hpp to LGPLv3

--- cc3d.hpp
+++ cc3d.hpp
@@ -15,28 +15,12 @@
  * 
  * https://github.com/seung-lab/connected-components-3d
  *
- *  The MIT License (MIT)
- *
- *  Copyright © 2020 William Silversmith
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a 
- *  copy of this software and associated documentation files (the “Software”), 
- *  to deal in the Software without restriction, including without limitation 
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- *  and/or sell copies of the Software, and to permit persons to whom the 
- *  Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all 
- * copies or substantial portions of the Software.
+ *  License: GNU Lesser General Public License v3
+ *  Copyright 2021 William Silversmith
  * 
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * ----
+ *  See COPYING.LESSER for that license. If the file is missing,
+ *  you can find the text here: 
+ *  https://www.gnu.org/licenses/lgpl-3.0.en.html
  */
 
 #ifndef CC3D_SPECIAL_4_HPP
