33e665221bfacb3fa98fd08ad18a34c5270db97c
William Silversmith <william.silversmith@gmail.com>
2021-06-16 23:51:37 -0400

    feat(cli): adds steps and six flags

--- compresso_cli/cli.py
+++ compresso_cli/cli.py
@@ -6,10 +6,26 @@ import click
 import compresso
 import numpy as np
 
+class Tuple3(click.ParamType):
+  """A command line option type consisting of 3 comma-separated integers."""
+  name = 'tuple3'
+  def convert(self, value, param, ctx):
+    if isinstance(value, str):
+      try:
+        value = tuple(map(int, value.split(',')))
+      except ValueError:
+        self.fail(f"'{value}' does not contain a comma delimited list of 3 integers.")
+      if len(value) != 3:
+        self.fail(f"'{value}' does not contain a comma delimited list of 3 integers.")
+    return value
+  
+
 @click.command()
 @click.option("-c/-d", "--compress/--decompress", default=True, is_flag=True, help="Decompress to a numpy .npy file.", show_default=True)
+@click.option('--steps', type=Tuple3(), default="4,4,1", help="Compression step size. No effect on decompression.", show_default=True)
+@click.option('--six', is_flag=True, default=False, help="Use 6-way CCL instead of 4-way. No effect on decompression.", show_default=True)
 @click.argument("source", nargs=-1)
-def main(compress, source):
+def main(compress, source, steps, six):
 	"""
 	Compress and decompress compresso files to and from numpy .npy files.
 
@@ -25,13 +41,16 @@ def main(compress, source):
 	This program was written by Brian Matejek and William Silversmith
 	and is MIT licensed.
 	"""
+	if len(steps) != 3:
+		raise ValueError(f"steps must be a comma delimited set of three numbers that multiply to > 0 and <= 64.")
+
 	for i in range(len(source)):
 		if source[i] == "-":
 			source = source[:i] + sys.stdin.readlines() + source[i+1:]
 	
 	for src in source:
 		if compress:
-			compress_file(src)
+			compress_file(src, steps, six)
 		else:
 			decompress_file(src)
 
@@ -65,14 +84,18 @@ def decompress_file(src):
 		print(f"compresso: Unable to write {dest}. Aborting.")
 		sys.exit()
 
-def compress_file(src):
+def compress_file(src, steps, six):
 	try:
 		data = np.load(src)
 	except ValueError:
 		print(f"compresso: {src} is not a numpy file.")
 		sys.exit()
 
-	binary = compresso.compress(data)
+	connectivity = 4 
+	if six:
+		connectivity = 6
+
+	binary = compresso.compress(data, steps=steps, connectivity=connectivity)
 	del data
 
 	dest = f"{src}.cpso"
