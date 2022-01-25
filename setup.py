cd1616da9b7a482bca0b4a221d9f427f6841cd4b
William Silversmith <william.silversmith@gmail.com>
2022-01-25 00:09:39 -0500

    build: ensure windows builds in release mode

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 import setuptools
+import sys
 
 import numpy as np
+
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++11', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++11', '-O3'
+  ]
 
 setuptools.setup(
   setup_requires=['pbr', 'numpy'],
@@ -10,7 +21,7 @@ setuptools.setup(
       'compresso',
       include_dirs=[ np.get_include() ],
       sources=['compresso.cpp'],
-      extra_compile_args=['-O3', '-std=c++11'],
+      extra_compile_args=extra_compile_args,
       language='c++'
     )
   ],
